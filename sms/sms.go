cd673f0125d58dd8b9f31f2d4724caacef7ac2b9
Dominik Menke <dom@digineo.de>
2022-02-18 14:21:07 +0100

    refactor: split large switch statements
    Move case body in separate methods:
    
    - (*Message).PDU() → (*Message).encodeXXX
    - (*Message).ReadFrom() → (*Message).decodeXXX
    
    Also extract User-Data decoding to reduce code duplication.

--- sms/sms.go
+++ sms/sms.go
@@ -77,137 +77,145 @@ func (s *Message) PDU() (int, []byte, error) {
 		buf.Write(octets)
 	}
 
+	var n int
+	var err error
+
 	switch s.Type {
 	case MessageTypes.Deliver:
-		var sms smsDeliver
-		sms.MessageTypeIndicator = byte(s.Type)
-		sms.MoreMessagesToSend = s.MoreMessagesToSend
-		sms.LoopPrevention = s.LoopPrevention
-		sms.ReplyPath = s.ReplyPathExists
-		sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
-		sms.StatusReportIndication = s.StatusReportIndication
-
-		addrLen, addr, err := s.Address.PDU()
-		if err != nil {
-			return 0, nil, err
-		}
-		var addrBuf bytes.Buffer
-		addrBuf.WriteByte(byte(addrLen))
-		addrBuf.Write(addr)
-		sms.OriginatingAddress = addrBuf.Bytes()
-
-		sms.ProtocolIdentifier = 0x00 // Short Message Type 0
-		sms.DataCodingScheme = byte(s.Encoding)
-		sms.ServiceCentreTimestamp = s.ServiceCenterTime.PDU()
-
-		var userData []byte
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			userData = pdu.Encode7Bit(s.Text)
-			sms.UserDataLength = byte(len(s.Text))
-		case Encodings.UCS2:
-			userData = pdu.EncodeUcs2(s.Text)
-			sms.UserDataLength = byte(len(userData))
-		default:
-			return 0, nil, ErrUnknownEncoding
-		}
-
-		sms.UserData = userData
-		n, err := buf.Write(sms.Bytes())
-		if err != nil {
-			return 0, nil, err
-		}
-		return n, buf.Bytes(), nil
+		n, err = s.encodeDeliver(&buf)
 	case MessageTypes.Submit:
-		var sms smsSubmit
-		sms.MessageTypeIndicator = byte(s.Type)
-		sms.RejectDuplicates = s.RejectDuplicates
-		sms.ValidityPeriodFormat = byte(s.VPFormat)
-		sms.ReplyPath = s.ReplyPathExists
-		sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
-		sms.StatusReportRequest = s.StatusReportRequest
-		sms.MessageReference = s.MessageReference
-
-		addrLen, addr, err := s.Address.PDU()
-		if err != nil {
-			return 0, nil, err
-		}
-		var addrBuf bytes.Buffer
-		addrBuf.WriteByte(byte(addrLen))
-		addrBuf.Write(addr)
-		sms.DestinationAddress = addrBuf.Bytes()
-
-		sms.ProtocolIdentifier = 0x00 // Short Message Type 0
-		sms.DataCodingScheme = byte(s.Encoding)
-
-		switch s.VPFormat {
-		case ValidityPeriodFormats.Relative:
-			sms.ValidityPeriod = byte(s.VP.Octet())
-		case ValidityPeriodFormats.Absolute, ValidityPeriodFormats.Enhanced:
-			return 0, nil, ErrNonRelative
-		}
-
-		var userData []byte
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			userData = pdu.Encode7Bit(s.Text)
-			sms.UserDataLength = byte(len(s.Text))
-		case Encodings.UCS2:
-			userData = pdu.EncodeUcs2(s.Text)
-			sms.UserDataLength = byte(len(userData))
-		default:
-			return 0, nil, ErrUnknownEncoding
-		}
-
-		sms.UserData = userData
-		n, err := buf.Write(sms.Bytes())
-		if err != nil {
-			return 0, nil, err
-		}
-		return n, buf.Bytes(), nil
+		n, err = s.encodeSubmit(&buf)
 	case MessageTypes.StatusReport:
-		var sms smsStatusReport
-		sms.MessageTypeIndicator = byte(s.Type)
-		sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
-		sms.MoreMessagesToSend = s.MoreMessagesToSend
-		sms.LoopPrevention = s.LoopPrevention
-		sms.StatusReportQualificator = s.StatusReportQualificator
-		sms.MessageReference = s.MessageReference
-
-		addrLen, addr, err := s.Address.PDU()
-		if err != nil {
-			return 0, nil, err
-		}
-		var addrBuf bytes.Buffer
-		addrBuf.WriteByte(byte(addrLen))
-		addrBuf.Write(addr)
-		sms.DestinationAddress = addrBuf.Bytes()
-
-		sms.ServiceCentreTimestamp = s.ServiceCenterTime.PDU()
-		sms.DischargeTimestamp = s.DischargeTime.PDU()
-		sms.Status = s.Status
-
-		var userData []byte
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			userData = pdu.Encode7Bit(s.Text)
-			sms.UserDataLength = byte(len(s.Text))
-		case Encodings.UCS2:
-			userData = pdu.EncodeUcs2(s.Text)
-			sms.UserDataLength = byte(len(userData))
-		default:
-			return 0, nil, ErrUnknownEncoding
-		}
-
-		sms.UserData = userData
-		n, err := buf.Write(sms.Bytes())
-		if err != nil {
-			return 0, nil, err
-		}
-		return n, buf.Bytes(), nil
+		n, err = s.encodeStatusReport(&buf)
 	default:
-		return 0, nil, ErrUnknownMessageType
+		err = ErrUnknownMessageType
 	}
+
+	if err != nil {
+		return 0, nil, err
+	}
+	return n, buf.Bytes(), nil
+}
+
+func (s *Message) encodeDeliver(buf *bytes.Buffer) (n int, err error) {
+	var sms smsDeliver
+	sms.MessageTypeIndicator = byte(s.Type)
+	sms.MoreMessagesToSend = s.MoreMessagesToSend
+	sms.LoopPrevention = s.LoopPrevention
+	sms.ReplyPath = s.ReplyPathExists
+	sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
+	sms.StatusReportIndication = s.StatusReportIndication
+
+	addrLen, addr, err := s.Address.PDU()
+	if err != nil {
+		return 0, err
+	}
+	var addrBuf bytes.Buffer
+	addrBuf.WriteByte(byte(addrLen))
+	addrBuf.Write(addr)
+	sms.OriginatingAddress = addrBuf.Bytes()
+
+	sms.ProtocolIdentifier = 0x00 // Short Message Type 0
+	sms.DataCodingScheme = byte(s.Encoding)
+	sms.ServiceCentreTimestamp = s.ServiceCenterTime.PDU()
+
+	var userData []byte
+	switch s.Encoding {
+	case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
+		userData = pdu.Encode7Bit(s.Text)
+		sms.UserDataLength = byte(len(s.Text))
+	case Encodings.UCS2:
+		userData = pdu.EncodeUcs2(s.Text)
+		sms.UserDataLength = byte(len(userData))
+	default:
+		return 0, ErrUnknownEncoding
+	}
+
+	sms.UserData = userData
+	return buf.Write(sms.Bytes())
+}
+
+func (s *Message) encodeSubmit(buf *bytes.Buffer) (n int, err error) {
+	var sms smsSubmit
+	sms.MessageTypeIndicator = byte(s.Type)
+	sms.RejectDuplicates = s.RejectDuplicates
+	sms.ValidityPeriodFormat = byte(s.VPFormat)
+	sms.ReplyPath = s.ReplyPathExists
+	sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
+	sms.StatusReportRequest = s.StatusReportRequest
+	sms.MessageReference = s.MessageReference
+
+	addrLen, addr, err := s.Address.PDU()
+	if err != nil {
+		return 0, err
+	}
+	var addrBuf bytes.Buffer
+	addrBuf.WriteByte(byte(addrLen))
+	addrBuf.Write(addr)
+	sms.DestinationAddress = addrBuf.Bytes()
+
+	sms.ProtocolIdentifier = 0x00 // Short Message Type 0
+	sms.DataCodingScheme = byte(s.Encoding)
+
+	switch s.VPFormat {
+	case ValidityPeriodFormats.Relative:
+		sms.ValidityPeriod = byte(s.VP.Octet())
+	case ValidityPeriodFormats.Absolute, ValidityPeriodFormats.Enhanced:
+		return 0, ErrNonRelative
+	}
+
+	var userData []byte
+	switch s.Encoding {
+	case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
+		userData = pdu.Encode7Bit(s.Text)
+		sms.UserDataLength = byte(len(s.Text))
+	case Encodings.UCS2:
+		userData = pdu.EncodeUcs2(s.Text)
+		sms.UserDataLength = byte(len(userData))
+	default:
+		return 0, ErrUnknownEncoding
+	}
+
+	sms.UserData = userData
+	return buf.Write(sms.Bytes())
+}
+
+func (s *Message) encodeStatusReport(buf *bytes.Buffer) (n int, err error) {
+	var sms smsStatusReport
+	sms.MessageTypeIndicator = byte(s.Type)
+	sms.UserDataHeaderIndicator = s.UserDataStartsWithHeader
+	sms.MoreMessagesToSend = s.MoreMessagesToSend
+	sms.LoopPrevention = s.LoopPrevention
+	sms.StatusReportQualificator = s.StatusReportQualificator
+	sms.MessageReference = s.MessageReference
+
+	addrLen, addr, err := s.Address.PDU()
+	if err != nil {
+		return 0, err
+	}
+	var addrBuf bytes.Buffer
+	addrBuf.WriteByte(byte(addrLen))
+	addrBuf.Write(addr)
+	sms.DestinationAddress = addrBuf.Bytes()
+
+	sms.ServiceCentreTimestamp = s.ServiceCenterTime.PDU()
+	sms.DischargeTimestamp = s.DischargeTime.PDU()
+	sms.Status = byte(s.Status)
+
+	var userData []byte
+	switch s.Encoding {
+	case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
+		userData = pdu.Encode7Bit(s.Text)
+		sms.UserDataLength = byte(len(s.Text))
+	case Encodings.UCS2:
+		userData = pdu.EncodeUcs2(s.Text)
+		sms.UserDataLength = byte(len(userData))
+	default:
+		return 0, ErrUnknownEncoding
+	}
+
+	sms.UserData = userData
+	return buf.Write(sms.Bytes())
 }
 
 // ReadFrom constructs a message from the supplied PDU octets. Returns the number of bytes read.
@@ -239,126 +247,114 @@ func (s *Message) ReadFrom(octets []byte) (n int, err error) {
 	buf.UnreadByte()
 	s.Type = MessageType(msgType & 0x03)
 
+	var decBytes int
+	octets = octets[1+scLen:]
+
 	switch s.Type {
 	case MessageTypes.Deliver:
-		var sms smsDeliver
-		off, err2 := sms.FromBytes(octets[1+scLen:])
-		n += off
-		if err2 != nil {
-			return n, err2
-		}
-		s.MoreMessagesToSend = sms.MoreMessagesToSend
-		s.LoopPrevention = sms.LoopPrevention
-		s.ReplyPathExists = sms.ReplyPath
-		s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
-		if sms.UserDataHeaderIndicator {
-			err = s.UserDataHeader.ReadFrom(sms.UserData)
-			if err != nil {
-				return
-			}
-		}
-		s.StatusReportIndication = sms.StatusReportIndication
-		s.Address.ReadFrom(sms.OriginatingAddress[1:])
-		s.Encoding = Encoding(sms.DataCodingScheme)
-		s.ServiceCenterTime.ReadFrom(sms.ServiceCentreTimestamp)
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			s.Text, err = pdu.Decode7Bit(sms.UserData)
-			if err != nil {
-				return
-			}
-			s.Text = cutStr(s.Text, int(sms.UserDataLength))
-		case Encodings.UCS2:
-			s.Text, err = pdu.DecodeUcs2(sms.UserData, s.UserDataStartsWithHeader)
-			if err != nil {
-				return
-			}
-		default:
-			return 0, ErrUnknownEncoding
-		}
+		decBytes, err = s.decodeDeliver(octets)
 	case MessageTypes.Submit:
-		var sms smsSubmit
-		off, err2 := sms.FromBytes(octets[1+scLen:])
-		n += off
-		if err2 != nil {
-			return n, err2
-		}
-		s.RejectDuplicates = sms.RejectDuplicates
-
-		switch s.VPFormat {
-		case ValidityPeriodFormats.Absolute, ValidityPeriodFormats.Enhanced:
-			return n, ErrNonRelative
-		default:
-			s.VPFormat = ValidityPeriodFormat(sms.ValidityPeriodFormat)
-		}
-
-		s.MessageReference = sms.MessageReference
-		s.ReplyPathExists = sms.ReplyPath
-		s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
-		s.StatusReportRequest = sms.StatusReportRequest
-		s.Address.ReadFrom(sms.DestinationAddress[1:])
-		s.Encoding = Encoding(sms.DataCodingScheme)
-
-		if s.VPFormat != ValidityPeriodFormats.FieldNotPresent {
-			s.VP.ReadFrom(sms.ValidityPeriod)
-		}
-
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			s.Text, err = pdu.Decode7Bit(sms.UserData)
-			if err != nil {
-				return
-			}
-			s.Text = cutStr(s.Text, int(sms.UserDataLength))
-		case Encodings.UCS2:
-			s.Text, err = pdu.DecodeUcs2(sms.UserData, s.UserDataStartsWithHeader)
-			if err != nil {
-				return
-			}
-		default:
-			return 0, ErrUnknownEncoding
-		}
+		decBytes, err = s.decodeSubmit(octets)
 	case MessageTypes.StatusReport:
-		var sms smsStatusReport
-		off, err2 := sms.FromBytes(octets[1+scLen:])
-		n += off
-		if err2 != nil {
-			return n, err2
-		}
-		s.MessageReference = sms.MessageReference
-		s.MoreMessagesToSend = sms.MoreMessagesToSend
-		s.LoopPrevention = sms.LoopPrevention
-		s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
-		if sms.UserDataHeaderIndicator {
-			err = s.UserDataHeader.ReadFrom(sms.UserData)
-			if err != nil {
-				return
-			}
-		}
-		s.StatusReportQualificator = sms.StatusReportQualificator
-		s.Status = sms.Status
-		s.Address.ReadFrom(sms.DestinationAddress[1:])
-		s.Encoding = Encoding(sms.DataCodingScheme)
-		s.ServiceCenterTime.ReadFrom(sms.ServiceCentreTimestamp)
-		s.DischargeTime.ReadFrom(sms.DischargeTimestamp)
-		switch s.Encoding {
-		case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
-			s.Text, err = pdu.Decode7Bit(sms.UserData)
-			if err != nil {
-				return
-			}
-			s.Text = cutStr(s.Text, int(sms.UserDataLength))
-		case Encodings.UCS2:
-			s.Text, err = pdu.DecodeUcs2(sms.UserData, s.UserDataStartsWithHeader)
-			if err != nil {
-				return
-			}
-		default:
-			return 0, ErrUnknownEncoding
-		}
+		decBytes, err = s.decodeStatusReport(octets)
 	default:
 		return n, ErrUnknownMessageType
 	}
 
-	return n, nil
+	n += decBytes
+	return n, err
+}
+
+func (s *Message) decodeDeliver(data []byte) (n int, err error) {
+	var sms smsDeliver
+	n, err = sms.FromBytes(data)
+	if err != nil {
+		return
+	}
+	s.MoreMessagesToSend = sms.MoreMessagesToSend
+	s.LoopPrevention = sms.LoopPrevention
+	s.ReplyPathExists = sms.ReplyPath
+	s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
+	if sms.UserDataHeaderIndicator {
+		err = s.UserDataHeader.ReadFrom(sms.UserData)
+		if err != nil {
+			return
+		}
+	}
+	s.StatusReportIndication = sms.StatusReportIndication
+	s.Address.ReadFrom(sms.OriginatingAddress[1:])
+	s.Encoding = Encoding(sms.DataCodingScheme)
+	s.ServiceCenterTime.ReadFrom(sms.ServiceCentreTimestamp)
+	err = s.decodeUserData(sms.UserData, sms.UserDataLength)
+	return n, err
+}
+
+func (s *Message) decodeSubmit(data []byte) (n int, err error) {
+	var sms smsSubmit
+	n, err = sms.FromBytes(data)
+	if err != nil {
+		return
+	}
+	s.RejectDuplicates = sms.RejectDuplicates
+
+	switch s.VPFormat {
+	case ValidityPeriodFormats.Absolute, ValidityPeriodFormats.Enhanced:
+		return n, ErrNonRelative
+	default:
+		s.VPFormat = ValidityPeriodFormat(sms.ValidityPeriodFormat)
+	}
+
+	s.MessageReference = sms.MessageReference
+	s.ReplyPathExists = sms.ReplyPath
+	s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
+	s.StatusReportRequest = sms.StatusReportRequest
+	s.Address.ReadFrom(sms.DestinationAddress[1:])
+	s.Encoding = Encoding(sms.DataCodingScheme)
+
+	if s.VPFormat != ValidityPeriodFormats.FieldNotPresent {
+		s.VP.ReadFrom(sms.ValidityPeriod)
+	}
+	err = s.decodeUserData(sms.UserData, sms.UserDataLength)
+	return n, err
+}
+
+func (s *Message) decodeStatusReport(data []byte) (n int, err error) {
+	var sms smsStatusReport
+	n, err = sms.FromBytes(data)
+	if err != nil {
+		return
+	}
+	s.MessageReference = sms.MessageReference
+	s.MoreMessagesToSend = sms.MoreMessagesToSend
+	s.LoopPrevention = sms.LoopPrevention
+	s.UserDataStartsWithHeader = sms.UserDataHeaderIndicator
+	if sms.UserDataHeaderIndicator {
+		err = s.UserDataHeader.ReadFrom(sms.UserData)
+		if err != nil {
+			return
+		}
+	}
+	s.StatusReportQualificator = sms.StatusReportQualificator
+	s.Status = sms.Status
+	s.Address.ReadFrom(sms.DestinationAddress[1:])
+	s.Encoding = Encoding(sms.DataCodingScheme)
+	s.ServiceCenterTime.ReadFrom(sms.ServiceCentreTimestamp)
+	s.DischargeTime.ReadFrom(sms.DischargeTimestamp)
+	err = s.decodeUserData(sms.UserData, sms.UserDataLength)
+	return n, err
+}
+
+func (s *Message) decodeUserData(data []byte, dataLen byte) (err error) {
+	switch s.Encoding {
+	case Encodings.Gsm7Bit, Encodings.Gsm7Bit_2:
+		if s.Text, err = pdu.Decode7Bit(data); err != nil {
+			return
+		}
+		s.Text = cutStr(s.Text, int(dataLen))
+	case Encodings.UCS2:
+		s.Text, err = pdu.DecodeUcs2(data, s.UserDataStartsWithHeader)
+	default:
+		return ErrUnknownEncoding
+	}
+	return err
 }
