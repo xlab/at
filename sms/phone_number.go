f08e6ce7f58b9914f2f80dd23be7464711db2264
Dominik Menke <dom@digineo.de>
2022-02-20 01:17:13 +0100

    sms: add known values for PhoneNumber numbering plan and type-of-address

--- sms/phone_number.go
+++ sms/phone_number.go
@@ -2,6 +2,7 @@ package sms
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -10,6 +11,61 @@ import (
 
 // PhoneNumber represents the address in either local or international format.
 type PhoneNumber string
+
+// PhoneNumberType represents Type-of-Number, as specified in 3GPP
+// TS 23.040 version 16.0.0 release 16, section 9.1.2.5.
+type PhoneNumberType byte
+
+// PhoneNumberTypes are all known PhoneNumberType values.
+var PhoneNumberTypes = struct {
+	Unknown         PhoneNumberType
+	International   PhoneNumberType
+	National        PhoneNumberType
+	NetworkSpecific PhoneNumberType
+	Subscriber      PhoneNumberType
+	Alphanumeric    PhoneNumberType
+	Abbreviated     PhoneNumberType
+	Reserved        PhoneNumberType // for future extension
+}{
+	Unknown:         0 << 4,
+	International:   1 << 4,
+	National:        2 << 4,
+	NetworkSpecific: 3 << 4,
+	Subscriber:      4 << 4,
+	Alphanumeric:    5 << 4,
+	Abbreviated:     6 << 4,
+	Reserved:        7 << 4,
+}
+
+// NumberingPlan represents Numbering-plan-identification, as specified
+// in 3GPP TS 23.040 version 16.0.0 release 16, section 9.1.2.5.
+type NumberingPlan byte
+
+// NumberingPlans are all known NumberingPlan valus. Other values are
+// reserved.
+var NumberingPlans = struct {
+	Unknown                NumberingPlan
+	E164                   NumberingPlan // ISDN/telephone numbering plan
+	X121                   NumberingPlan // Data numbering plan
+	Telex                  NumberingPlan
+	ServiceCentreSpecificA NumberingPlan // used to indicate a numbering plan specific to ESME attached to the SMSC
+	ServiceCentreSpecificB NumberingPlan // used to indicate a numbering plan specific to ESME attached to the SMSC
+	National               NumberingPlan
+	Private                NumberingPlan
+	ERMES                  NumberingPlan
+	Reserved               NumberingPlan // for future extension
+}{
+	Unknown:                0b0000,
+	E164:                   0b0001,
+	X121:                   0b0011,
+	Telex:                  0b0100,
+	ServiceCentreSpecificA: 0b0101,
+	ServiceCentreSpecificB: 0b0110,
+	National:               0b1000,
+	Private:                0b1001,
+	ERMES:                  0b1010,
+	Reserved:               0b1111,
+}
 
 // PDU returns the number of digits in address and octets of semi-octet encoded address.
 func (p PhoneNumber) PDU() (int, []byte, error) {
@@ -31,37 +87,40 @@ func (p PhoneNumber) PDU() (int, []byte, error) {
 	return n, buf.Bytes(), nil
 }
 
-// Type returns the type of address — local or international.
+// Type returns the type of address (a combination of type-of-number and
+// numbering-plan-identification). Currently, only national and
+// international E.164 numbers are understood. While ReadFrom() can
+// parse alphanumeric numbers, Type() doesn't recognize it.
 func (p PhoneNumber) Type() byte {
+	typ := PhoneNumberTypes.National
 	if strings.HasPrefix(string(p), "+") {
-		return (0x81 | 0x10) // 1001 0001
+		typ = PhoneNumberTypes.International
 	}
-	return (0x81 | 0x20) // 1010 0001
+	return 0x80 | byte(typ) | byte(NumberingPlans.E164)
 }
 
 // ReadFrom constructs an address from the semi-decoded version in the supplied byte slice.
-func (p *PhoneNumber) ReadFrom(octets []byte) {
+func (p *PhoneNumber) ReadFrom(octets []byte) error {
 	if len(octets) < 1 {
-		return
+		return ErrIncorrectSize
 	}
-	addrType := octets[0]
 
-	// Alphanumeric, (coded according to GSM TS 03.38 7-bit default alphabet)
-	if addrType&0x70 == 0x50 {
-		// decode 7 bit
+	typ := PhoneNumberType(octets[0] & 0b0111_0000)
+	switch typ {
+	case PhoneNumberTypes.Alphanumeric:
 		addr, err := pdu.Decode7Bit(octets[1:])
 		if err != nil {
-			// handle error, panic or log.Println("Decode7bit", octets, err)
+			return err
 		}
 		*p = PhoneNumber(addr)
-		return
-	}
-
-	addr := pdu.DecodeSemiAddress(octets[1:])
-	if addrType&0x10 > 0 {
+	case PhoneNumberTypes.International:
+		addr := pdu.DecodeSemiAddress(octets[1:])
 		*p = PhoneNumber("+" + addr)
-	} else {
+	case PhoneNumberTypes.National:
+		addr := pdu.DecodeSemiAddress(octets[1:])
 		*p = PhoneNumber(addr)
+	default:
+		return fmt.Errorf("%w: Type(0x%x)", ErrUnsupportedTypeOfNumber, typ)
 	}
-	return
+	return nil
 }
