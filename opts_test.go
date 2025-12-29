10b967173f08bd3007c37f42b17a8dc80d5cef5f
Max Kupriianov <max@kc.vc>
2025-12-29 13:54:06 +0400

    chore: fix opts as suggested in https://github.com/xlab/at/pull/40

--- opts_test.go
+++ opts_test.go
@@ -23,4 +23,28 @@ func TestResolve(t *testing.T) {
 	assert.Equal(t, opt, ServiceStates.Resolve(1))
 }
 
+// Test DeleteOptions field access and resolve function.
+func TestDeleteOptions(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, 0, DeleteOptions.Index.ID)
+	assert.Equal(t, "Delete message by index", DeleteOptions.Index.Description)
+	assert.Equal(t, 4, DeleteOptions.All.ID)
+	assert.Equal(t, "Delete all messages", DeleteOptions.All.Description)
+	assert.Equal(t, DeleteOptions.Index, DeleteOptions.Resolve(0))
+	assert.Equal(t, DeleteOptions.All, DeleteOptions.Resolve(4))
+}
+
+// Test MessageFlags field access and resolve function.
+func TestMessageFlags(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, 0, MessageFlags.Unread.ID)
+	assert.Equal(t, "Unread", MessageFlags.Unread.Description)
+	assert.Equal(t, 4, MessageFlags.Any.ID)
+	assert.Equal(t, "Any", MessageFlags.Any.Description)
+	assert.Equal(t, MessageFlags.Unread, MessageFlags.Resolve(0))
+	assert.Equal(t, MessageFlags.Any, MessageFlags.Resolve(4))
+}
+
 // TODO: complete this suite in case of 100% coverage needed.
